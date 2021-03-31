208a65b8340475430784743e98369c29c1b845c6
Celian RAIMBAULT <celian.raimbault@epita.fr>
2021-03-31 17:23:06 +0100

    object: Added some builtin methods

--- src/object.cc
+++ src/object.cc
@@ -1,12 +1,17 @@
 #include "object.hh"
-#include "program.hh"
 #include "error.hh"
+#include "program.hh"
 #include <iostream>
 
 using namespace std;
 
 // --- Object ---
 Type *Object::class_type = nullptr;
+
+void Object::init_class_type() {
+    class_type = new Type("Object");
+    // TODO : Str
+}
 
 Object::Object(Type *type) : type(type) {
     init_gc_data(this);
@@ -23,9 +28,42 @@ void Object::traverse_objects(const fn_visit_object_t &visit) {
         type->fn_traverse_objects(this, visit);
 }
 
-void Object::init_class_type() {
-    class_type = new Type("Object");
+// Throws a NameError that says no such builtin method
+#define THROW_NOBUILTIN(METHOD)                                                \
+    throw_fmt(NameError, "Type '%s' has no @" #METHOD " method",               \
+              type->name.c_str());
+
+Object *Object::index(Object *args) {
+    if (!type->fn_index) {
+        THROW_NOBUILTIN(index);
+
+        return nullptr;
+    }
+
+    return type->fn_index(this, args);
 }
+
+Object *Object::hash() {
+    if (!type->fn_hash) {
+        THROW_NOBUILTIN(hash);
+
+        return nullptr;
+    }
+
+    return type->fn_hash(this);
+}
+
+Object *Object::str() {
+    if (!type->fn_str) {
+        THROW_NOBUILTIN(str)
+
+        return nullptr;
+    }
+
+    return type->fn_str(this);
+}
+
+#undef THROW_NOBUILTIN
 
 // --- Type ---
 static int type_global_id = 0;
@@ -34,7 +72,8 @@ Type *Type::class_type = nullptr;
 
 Type::Type(const str_t &name) : Object(Type::class_type), name(name) {
     // Register this type to the program
-    if (Program::instance) Program::add_type(this);
+    if (Program::instance)
+        Program::add_type(this);
 
     id = ++type_global_id;
 }
@@ -46,9 +85,7 @@ void Type::init_class_type() {
     class_type->type = class_type;
 }
 
-bool Type::operator==(const Type &other) const {
-    return id == other.id;
-}
+bool Type::operator==(const Type &other) const { return id == other.id; }
 
 // --- TODO ---
 #include "map.hh"
@@ -57,7 +94,7 @@ struct TestType;
 static TestType *test_type = nullptr;
 
 struct TestObject : public Object {
-    TestObject(const str_t &id = "?") : Object((Type*)test_type), id(id) {}
+    TestObject(const str_t &id = "?") : Object((Type *)test_type), id(id) {}
 
     ~TestObject() { cout << id << " deleted" << endl; }
 
@@ -87,5 +124,6 @@ void testObjects() {
 
     cout << "Index 1 : " << map->fn_index(key) << endl;
     cout << "Index 2 : " << map->fn_index(value) << endl;
-    cerr << "Error : " << ((Error*)Program::instance->current_error)->msg << endl;
+    cerr << "Error : " << ((Error *)Program::instance->current_error)->msg
+         << endl;
 }
--- src/object.hh
+++ src/object.hh
@@ -27,17 +27,23 @@ struct Object {
 
     Type *type;
 
-    virtual ~Object();
-
-    // Wrappers of Type::fn_*
-    void traverse_objects(const fn_visit_object_t &visit);
-
     Object(Type *type);
 
     // This static function inits the static attribute class_type
     // It is present on other objects such as Type
     // This function should be called in init_types
+    // Can throw for most objects
     static void init_class_type();
+
+    virtual ~Object();
+
+    // Built-in methods, wrappers of Type::fn_*
+    // See Type::fn_* for more details
+    // Can throw and return nullptr (except for traverse_objects)
+    void traverse_objects(const fn_visit_object_t &visit);
+    Object *index(Object *args);
+    Object *hash();
+    Object *str();
 };
 
 // Every type must have a unique instance of this class
@@ -51,14 +57,23 @@ struct Type : public Object {
     str_t name;
 
     // The tp_traverse function
-    // Can be empty
     fn_traverse_objects_t fn_traverse_objects;
 
-    // TODO :
-    // fn_unary_t fn_new;
+    // Subscript, example : obj[42]
+    fn_binary_t fn_index;
+
+    // Used for hash tables etc...
+    // Returns an Int
+    fn_unary_t fn_hash;
+
+    // String representation
+    // Returns a Str
+    fn_unary_t fn_str;
+
+    // TODO : + doc
+    // fn_unary_t fn_init;
     // fn_unary_t fn_del;
-    // fn_unary_t fn_hash;
-    // fn_unary_t fn_str;
+    // fn_binary_t fn_in;
     // fn_binary_t fn_cmp;
     // fn_binary_t fn_add;
     // fn_binary_t fn_sub;
@@ -68,11 +83,11 @@ struct Type : public Object {
     // fn_unary_t fn_call;
     // Map *attrs; // Can't be written if starts by @
 
-    // Compares the unique identifier
-    bool operator==(const Type &other) const;
-
     // A type is always global
     Type(const str_t &name);
 
     static void init_class_type();
+
+    // Compares the unique identifier
+    bool operator==(const Type &other) const;
 };
